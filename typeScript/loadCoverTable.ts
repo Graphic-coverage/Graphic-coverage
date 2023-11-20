f7a45097d24d2baaa6e19ab751fd8a69ed5e3457
JonatanRuvenes <116504321+JonatanRuvenes@users.noreply.github.com>
2023-11-20 14:09:49 +0200

    creating class and starting loading data from jason

---
+++ typeScript/loadCoverTable.ts
@@ -0,0 +1,10 @@
+import { teamCover } from './teamCover';
+
+let data: teamCover[] = [];
+
+function load(start: number, end: number){
+    for(var _i = 0; _i < end - start; _i++){
+        
+        data.push();
+    }
+}
---
+++ typeScript/teamCover.ts
@@ -0,0 +1,16 @@
+export class teamCover{
+    _teamName: string;
+    _teamNumber: string;
+    _RP: number;
+    _totalScore: number;
+    _averagePoints: number;
+    _averageRanking: number;
+    _totalAutonomous: number;
+    _autonomousAverageRank: number;
+    _totalTeleOp: number;
+    _averageTeleOp: number;
+    _TeleOpAverageRank: number;
+    _matchesPlayed: number;
+
+    
+}
