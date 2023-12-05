e3da780bd57d5ab88a179325152e42be50cb19bb
JonatanRuvenes <116504321+JonatanRuvenes@users.noreply.github.com>
2023-12-05 15:38:26 +0200

    its not working

--- typeScript/loadCoverTable.ts
+++ typeScript/loadCoverTable.ts
@@ -1,10 +1,30 @@
 import { teamCover } from './teamCover';
+import * as fs from 'fs/promises';
 
 let data: teamCover[] = [];
 
-function load(start: number, end: number){
-    for(var _i = 0; _i < end - start; _i++){
-        
-        data.push();
+async function load(start: number, end: number) {
+    try {
+        // Read JSON file
+        const jsonData = await fs.readFile('./jsons/teamsCover.json', 'utf8');
+        const teams = JSON.parse(jsonData).teams;
+
+        // Iterate through the specified range and insert data into the array
+        for (let i = start; i <= end; i++) {
+            const teamData = teams[i - 1]; // Adjust index since team numbers start from 1
+            const team_cover = new teamCover();
+
+            // Your existing code for populating team_cover...
+
+            data.push(team_cover);
+        }
+
+        // Example: Load teams from 1 to 5
+        await load(1, 5);
+
+        // Print the result
+        console.log(data);
+    } catch (error) {
+        console.error('Error reading file:', error);
     }
 }
---
+++ typeScript/teamCover.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.teamCover = void 0;
+var teamCover = /** @class */ (function () {
+    function teamCover() {
+    }
+    return teamCover;
+}());
+exports.teamCover = teamCover;
